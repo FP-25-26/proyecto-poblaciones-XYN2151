25aff885d4bd7e5d16563d1c13e2eedf893a13c1
XYN2151 <julgonzam@alum.us.es>
2025-10-22 15:58:37 +0200

    Implement population data processing and visualization functions

---
+++ src/poblacion_test.py
@@ -0,0 +1,5 @@
+from poblacion import *
+
+poblaciones = lee_poblaciones("D:\IS\FP\Laboratorio\poblaciones-XYN2151\data\population.csv")
+
+muestra_comparativa_paises_anyo(poblaciones, 1980, ['Spain', 'France', 'Germany'])
